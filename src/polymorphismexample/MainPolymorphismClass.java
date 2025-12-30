e1fd130cf600f875c98e964183b7649d1500ca9e
ahamadarbab <ahmadarbab.456@gmail.com>
2025-12-31 00:23:28 +0530

    Polymorphism example using method overloading

---
+++ src/polymorphismexample/MainPolymorphismClass.java
@@ -0,0 +1,14 @@
+package polymorphismexample;
+
+public class MainPolymorphismClass {
+    public static void main(String[] args) {
+
+        PerformSum performSum1 = new PerformSum();
+        performSum1.sum(2, 3);
+        performSum1.sum(4, 5, 6);
+        performSum1.sum(2, 4.56f);
+        performSum1.sum(5.65, 4.35f);
+        performSum1.sum(6.45f, 5.55f);
+        performSum1.sum(3.85, 5.15);
+    }
+}
